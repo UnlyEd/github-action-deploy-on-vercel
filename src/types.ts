d1d6800d3562ccfa83693c915e3afa539c11951b
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-05-06 11:21:38 +0200

    Add GenericObject (fixes TS warning)

--- src/types.ts
+++ src/types.ts
@@ -2,9 +2,9 @@ export type VercelConfig = {
   version?: number,
   name?: string,
   scope?: string,
-  env?: object,
+  env?: GenericObject,
   build?: {
-    env: object
+    env: GenericObject
   },
   alias: string[],
   regions?: string[],
@@ -34,3 +34,13 @@ export type VercelAliasResponse = {
   alias?: string,
   created?: string
 }
+
+/**
+ * Helper to avoid writing `Record<string, unknown>` everywhere you would usually use "object".
+ *
+ * @example (data: GenericObject) => void
+ * @example variables: GenericObject<string>
+ *
+ * @see https://github.com/typescript-eslint/typescript-eslint/issues/2063#issuecomment-632833366
+ */
+export type GenericObject<T = unknown> = Record<string, T>;
