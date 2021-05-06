d1c40db86a95a1a395ab8ae6b92c194c7daaec04
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-05-06 11:11:14 +0200

    Convert "require" to "import" + use pascalCasing + code breathe

--- src/vercel.ts
+++ src/vercel.ts
@@ -1,6 +1,6 @@
 import * as core from '@actions/core';
-import { ExecOptions } from '@actions/exec';
-import { Globber } from '@actions/glob';
+import exec, { ExecOptions } from '@actions/exec';
+import glob, { Globber } from '@actions/glob';
 import fs from 'fs';
 import fetch from 'node-fetch';
 import { VERCEL_CONFIG_FILE } from './config';
@@ -10,10 +10,7 @@ import {
   VercelConfig,
 } from './types';
 
-const exec = require('@actions/exec');
-const glob = require('@actions/glob');
-
-const generate_alias_promises = (deploymentId: string, teamId: string, aliases: string[]): Promise<VercelAliasResponse>[] => {
+const generateAliasPromises = (deploymentId: string, teamId: string, aliases: string[]): Promise<VercelAliasResponse>[] => {
   const aliasCreationPromises: Promise<VercelAliasResponse>[] = [];
   for (const alias of aliases) {
     console.log(`Creating alias ${alias}`);
@@ -31,7 +28,7 @@ const generate_alias_promises = (deploymentId: string, teamId: string, aliases: 
   return aliasCreationPromises;
 };
 
-const exec_command = async (command: string): Promise<string> => {
+const execCommand = async (command: string): Promise<string> => {
   /**
    * When we execute a program, it writes on two outputs : standard and error.
    * Initalizing empty variables to receive these outputs
@@ -55,39 +52,44 @@ const exec_command = async (command: string): Promise<string> => {
   return stdout;
 };
 
-const create_aliases = async (deploymentUrl: string, customDeploymentFile: string | undefined, failIfAliasNotLinked: boolean): Promise<void> => {
+const createAliases = async (deploymentUrl: string, customDeploymentFile: string, failIfAliasNotLinked: boolean): Promise<void> => {
   core.debug(`Starting to link aliases`);
-  /**
-   * Globber is a github action tool https://github.com/actions/toolkit/tree/master/packages/glob
-   * It helps us to find the absolute path for a file. Indeed, because we don't know where the action will be run and we need to find this file, wherever it is.
-   */
+
+  // Globber is a github action tool https://github.com/actions/toolkit/tree/master/packages/glob
+  // It helps us to find the absolute path for a file. Indeed, because we don't know where the action will be run and we need to find this file, wherever it is.
   const globber: Globber = await glob.create(customDeploymentFile);
   const vercelConfigFile: string = (await globber.glob())[0];
+
   if (vercelConfigFile && fs.existsSync(vercelConfigFile)) {
     core.debug(`Found custom config file: ${vercelConfigFile}`);
     core.debug(`Found real path: ${vercelConfigFile}`);
     const vercelConfig: VercelConfig = JSON.parse(fs.readFileSync(vercelConfigFile, 'utf8'));
+
     if (vercelConfig.alias) {
       const {
         id,
         ownerId,
-      } = (await fetch(`https://api.vercel.com/v11/now/deployments/get?url=${deploymentUrl.replace('https://', '')}`, {
+      } = await fetch(`https://api.vercel.com/v11/now/deployments/get?url=${deploymentUrl.replace('https://', '')}`, {
         headers: {
           Authorization: `Bearer ${process.env.VERCEL_TOKEN}`,
         },
         method: 'GET',
-      }).then((data) => data.json()));
-      const aliasCreationPromises: Promise<VercelAliasResponse>[] = generate_alias_promises(id, ownerId, vercelConfig.alias);
+      }).then((data) => data.json());
+
+      const aliasCreationPromises: Promise<VercelAliasResponse>[] = generateAliasPromises(id, ownerId, vercelConfig.alias);
       core.debug(`Resolving alias promises`);
       const aliasesResponse: VercelAliasResponse[] = await Promise.all<VercelAliasResponse>(aliasCreationPromises);
       console.log(`Alias creation response: ${JSON.stringify(aliasesResponse)}`);
+
       if (aliasesResponse.filter((response) => response.error).length > 0) {
         const failedAliases: (VercelAliasResponseError | undefined)[] = aliasesResponse.filter((response: VercelAliasResponse) => response.error).map((response) => response.error);
         const message = `Got following errors: ${JSON.stringify(failedAliases)}`;
+
         failIfAliasNotLinked ? core.setFailed(message) : core.warning(message);
         console.log(`Exporting this error...`);
         core.setOutput('VERCEL_ALIASES_ERROR', failedAliases);
       }
+
       for (const alias of aliasesResponse.filter((response) => !response.error)) {
         console.log(`Created alias ${alias}`);
       }
@@ -105,31 +107,33 @@ const deploy = async (command: string, deployAlias: boolean, failIfAliasNotLinke
    *
    * Running "exec_command" displays the output in the console.
    */
-  const stdout: string = await exec_command(command);
+  const stdout: string = await execCommand(command);
 
   /**
    * Parsing this huge output by using Regex match.
    * match function return strings matching with the pattern.
+   *
    * Pattern "/https?:\/\/[^ ]+.vercel.app/gi"
    *          "/https?\/\/:" start matching when we find http:// or https://
    *          "[^ ]+.vercel.app" will catch everything as a vercel subdomain, so "*.vercel.app"
    *          "/g" allows us to have many matchess
    *          "i" make the regex case insensitive. It will match for "https://subDomainApp.vercel.app" and "https://subdomainapp.vercel.app"
-   * shift returns the first occurence
+   *          "shift" returns the first occurence
    */
   const deploymentUrl: string | undefined = stdout.match(/https?:\/\/[^ ]+.vercel.app/gi)?.shift();
 
   /**
    * Locating any custom config file for Vercel (if you are using one file per customer or deployment for the same app)
    * match function return strings matching with the pattern.
+   *
    * Pattern "/--local-config=.[^$]+?.json/gs"
    *          "/--local-config=" starts matching on finding the argument local-config
    *          "[^$]+?.json" with a json file provided as value
    *          "/g" allows us to have many matchess
    *          "s" reduce match scope on the same line
-   * shift returns the first occurence
-   * split isolates the json file
-   * find automatically finds the matching json file
+   *          "shift" returns the first occurence
+   *          "split" isolates the json file
+   *          "find" automatically finds the matching json file
    */
   const customDeploymentFile: string = stdout.match(/--local-config=.[^$]+?.json/gs)?.shift()?.split('=').find((el) => el.endsWith('.json')) || VERCEL_CONFIG_FILE;
 
@@ -140,13 +144,16 @@ const deploy = async (command: string, deployAlias: boolean, failIfAliasNotLinke
     const deploymentDomain: string = deploymentUrl.replace('https://', '');
     console.log(`Found url deployment. Exporting it...`);
     console.log(`VERCEL_DEPLOYMENT_URL=${deploymentUrl}`);
+
     core.exportVariable('VERCEL_DEPLOYMENT_URL', deploymentUrl);
     core.setOutput('VERCEL_DEPLOYMENT_URL', deploymentUrl);
     console.log(`VERCEL_DEPLOYMENT_DOMAIN=${deploymentDomain}`);
+
     core.exportVariable('VERCEL_DEPLOYMENT_DOMAIN', deploymentDomain);
     core.setOutput('VERCEL_DEPLOYMENT_DOMAIN', deploymentDomain);
+
     if (deployAlias) {
-      await create_aliases(deploymentUrl, customDeploymentFile, failIfAliasNotLinked);
+      await createAliases(deploymentUrl, customDeploymentFile, failIfAliasNotLinked);
     }
   } else {
     core.setFailed(`"Error during command execution, cannot find any url matching (using a regex to retrieve a url as "https://*.vercel.app"`);
