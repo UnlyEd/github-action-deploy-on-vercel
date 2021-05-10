7776df1b603cb01aee3a41529d7139b203863fe0
Hugo MARTIN <hugo.martin.69@gmail.com>
2021-05-10 18:51:10 +0200

    Trying to understand how an array is retrieve in GHA

--- src/main.ts
+++ src/main.ts
@@ -26,10 +26,12 @@ const run = async (): Promise<void> => {
 
   try {
     const command: string = core.getInput('command');
+    const extraAliases: string = core.getInput('extraAliases');
     const applyDomainAliases: boolean = core.getInput('applyDomainAliases') == 'true';
     const failIfAliasNotLinked: boolean = core.getInput('failIfAliasNotLinked') == 'true';
     core.debug(`Received command: ${command}`); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true https://github.com/actions/toolkit/blob/master/docs/action-debugging.md#how-to-access-step-debug-logs
     core.debug(`Should we deploy aliases ? "${applyDomainAliases}"`);
+    console.log(`|${extraAliases}|`);
     await deploy(command, applyDomainAliases, failIfAliasNotLinked);
   } catch (error) {
     core.setFailed(error.message);
