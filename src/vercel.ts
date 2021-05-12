3177fe80d113396da2d77def454a2a1046f27550
Hugo MARTIN <hugo.martin.69@gmail.com>
2021-05-12 19:35:29 +0200

    Fixed unefined dynamic alias

--- src/vercel.ts
+++ src/vercel.ts
@@ -88,7 +88,7 @@ const createAliases = async (deploymentUrl: string, customDeploymentFile: string
         .then((data) => data.json())
         .catch((error) => core.warning(`Did not receive JSON from Vercel API while creating aliases. Message: ${error?.message}`));
 
-      const aliasAsked: string[] = [...vercelConfig.alias, ...extraAliases];
+      const aliasAsked: string[] = [...vercelConfig.alias, ...extraAliases].filter((alias) => alias ? true : false);
       core.debug(`List of aliases to apply: ${aliasAsked}`);
 
       const aliasCreationPromises: Promise<VercelAliasResponse>[] = generateAliasPromises(id, ownerId, aliasAsked);
