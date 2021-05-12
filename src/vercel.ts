8a505fffedaa914d21bf8e52bac998bf7f9e6de5
Hugo MARTIN <hugo.martin.69@gmail.com>
2021-05-12 18:39:26 +0200

    Got a working workflow

--- src/vercel.ts
+++ src/vercel.ts
@@ -89,8 +89,7 @@ const createAliases = async (deploymentUrl: string, customDeploymentFile: string
         .catch((error) => core.warning(`Did not receive JSON from Vercel API while creating aliases. Message: ${error?.message}`));
 
       const aliasAsked: string[] = [...vercelConfig.alias, ...extraAliases];
-
-      console.log("List of aliases you asked to create: ", aliasAsked);
+      core.debug(`List of aliases to apply: ${aliasAsked}`);
 
       const aliasCreationPromises: Promise<VercelAliasResponse>[] = generateAliasPromises(id, ownerId, aliasAsked);
       core.debug(`Resolving alias promises`);
